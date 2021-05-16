1968c271d57521d4a6e204d109d4ad7917d678ec
@BIuPAL <abiupal@mac.com>
2021-05-16 14:01:58 +0900

    #20210516 1st commit

---
+++ struct2json.py
@@ -0,0 +1,93 @@
+import json
+import sys
+
+is_inside_struct = False
+output_json = {}
+order_members = []
+members = {}
+offset = 0
+
+def setEndian():
+    #sys.stdout = codecs.getwriter('utf_8')(sys.stdout)
+    output_json["_Endian"] = "<" # Little for struct format
+
+def checkStruct(line):
+    global is_inside_struct
+    global output_json
+    global order_members
+    global order_index
+    global members
+    global offset
+    
+    # print(line)
+    if len(line) < 1:
+        return
+    sections = line.strip().split()
+    #print(sections)
+    if is_inside_struct == False:
+        if ("typedef" in sections) and ("struct" in sections):
+            is_inside_struct = True
+            order_members = []
+            order_index = 0
+            members = {}
+            offset = 0
+        return
+    elif sections[0] == '}':
+        is_inside_struct = False
+        struct_name = sections[1].strip(';')
+        output_json[struct_name] = {}
+        output_json[struct_name]['_order'] = order_members
+        output_json[struct_name]['_members']= members
+    
+    datatype = ''
+    name = ''
+    datasize = 1
+    pos = 0
+    if sections[pos] == "unsigned":
+        datatype += sections[pos] + ' '
+        pos = pos + 1
+    # Data TYpe 
+    checkTypes = ['char', 'short', 'long', 'int', 'float', 'double' ]
+    types_byteSize = {'char':1, 'short':2, 'long':4, 'int':4, 'float':4, 'double':8 }
+    if sections[pos] in checkTypes:
+        datatype += sections[pos]
+        type_datasize = types_byteSize[ sections[pos] ]
+        pos = pos + 1
+        vars = sections[pos].strip(';').split('[')
+        name = vars[0]
+        # print("vars:" + str(vars))
+        if 1 < len(vars):
+            # 配列数を取得
+            datasize = int(vars[1].strip(']'))
+        datasize = datasize * type_datasize
+        
+        #print( datatype + ',' + name + ',' + str(datasize), ',type:' + str(type_datasize))
+        
+        order_members.append(name)
+        members[name] = {}
+        members[name]['_offset'] = offset
+        members[name]['_bytesize'] = datasize
+        members[name]['_datatype'] = datatype
+        
+        offset = offset + datasize
+
+
+def main():
+    setEndian()
+    try:
+        line = input()
+        while True:
+            checkStruct(line)
+            line = input()
+    except Exception as e:
+        print(e)
+        #print(output_json)
+        with open("output.json", "w") as fp:
+            json.dump(output_json, fp
+            , ensure_ascii=False
+            , indent=4
+            , sort_keys=True
+            , separators=(',', ': '))
+
+if __name__ == "__main__":
+    main()
